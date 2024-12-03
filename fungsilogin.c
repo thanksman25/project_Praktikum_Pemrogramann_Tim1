898839a1db2ba246bd33e526db389b00816ecfd4
thanksman25 <abdan24@mhs.usk.ac.id>
2024-12-03 12:02:40 +0700

    Update fungsilogin.c

--- fungsilogin.c
+++ fungsilogin.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+void login() {
+    char akun; 
+    printf("Silakan masuk dengan akun (u untuk user, a untuk admin): ");
+    scanf(" %c", &akun);
+    if (akun == 'u') {
+        printf("Selmat datang user, berikut list alat lab yang tersedia\n");
+
+    } else if (akun == 'a') {
+        printf("Anda masuk sebagai admin.\n");
+    } else {
+        printf("Akun tidak dikenal. Silakan coba lagi.\n");
+    }
+}
