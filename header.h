4dd01e2271f2a22edfc9cb0897c35cb3dd58e677
annisarahmafathia <annisarahmafathia63@gmail.com>
2024-12-06 11:18:31 +0700

    Update header.h

--- header.h
+++ header.h
@@ -27,3 +27,34 @@ extern Akun akun[MAX_USER];
 
 // Fungsi untuk login
 int login(char *username, char *password);
+// Fungsi untuk membaca data alat dari file
+void readDataAlat();
+
+// Fungsi untuk menulis data alat ke file
+void writeDataAlat();
+
+// Fungsi untuk menampilkan alat lab
+void showAlatLab();
+
+// Fungsi untuk meminjam alat lab
+void pinjamAlatLab();
+
+// Fungsi untuk mengembalikan alat lab
+void kembalikanAlatLab();
+
+// Fungsi untuk admin menambah alat
+void tambahAlatLab();
+
+// Fungsi untuk admin menghapus alat
+void hapusAlatLab();
+
+// Fungsi untuk admin mengedit alat
+void editAlatLab();
+
+// Menu admin
+void menuAdmin();
+
+// Menu user
+void menuUser();
+
+#endif // HEADER_H
