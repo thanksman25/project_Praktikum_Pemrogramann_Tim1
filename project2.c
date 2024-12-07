be0427bcdf9738493d39388ac716f4a0ba603046
Almira Sadida <almira244@mhs.usk.ac.id>
2024-12-07 07:59:44 +0700

    Update project2.c

--- project2.c
+++ project2.c
@@ -5,26 +5,27 @@
 #define MAX_BARIS 256
 
 int main() {
-
     char baris[MAX_BARIS], kata[MAX_KATA][MAX_BARIS];
     int jumlah_kata = 0;
     int judul = 1;
     int unik = 1;
 
+    //membuka file input dan output
     FILE *input = fopen("lirik.txt", "r");
     FILE *output = fopen("kosa-kata.word", "w");
     if (!input || !output) {
         printf("Error: Tidak dapat membuka file.\n");
         return 1;
     }
-
+    //membaca baris dari file input
     while (fgets(baris, sizeof(baris), input)) {
         if (judul) { 
+            //menulis baris pertama ke file output
             fprintf(output, "%s", baris);
             judul = 0;
             continue;
         }
-
+        //memisahkan kata-kata dalam baris
         char *lirik = strtok(baris, " \n");
         while (lirik) {
             // Bersihkan kata
@@ -44,7 +45,7 @@ int main() {
                     break;
                 }
             }
-
+            //jika kata unik, ditambahkan ke daftar kata
             if (unik && strlen(bersih) > 0) {
                 strcpy(kata[jumlah_kata++], bersih);
             }
@@ -57,7 +58,7 @@ int main() {
     for (int i = 0; i < jumlah_kata; i++) {
         fprintf(output, "%s=\n\n", kata[i]);
     }
-
+    //menutup file input dan output
     fclose(input);
     fclose(output);
     return 0;
